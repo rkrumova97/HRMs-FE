54cd05b2bb4f5bb7a5a46aeb34a5f2df87f46173
rkrumova <Rumyana.Krumova@endava.com>
2019-06-15 21:21:15 +0300

    US:003 Get Skills and Grades from the BE

--- src/app/modules/hr/hire-employee/hire-employee.component.ts
+++ src/app/modules/hr/hire-employee/hire-employee.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-hire-employee',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HireEmployeeComponent implements OnInit {
 
-  constructor() { }
+  grades: string[];
+  http: HttpClient;
+  skills: string[];
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
 
   ngOnInit() {
+     this.http.get<string[]>('http://localhost:8080/grades').subscribe(res => {
+      this.grades = res;
+      console.log(this.grades);
+    });
+
+     this.http.get<string[]>('http://localhost:8080/skills').subscribe(res => {
+      this.skills = res;
+      console.log(this.skills);
+    });
   }
 
 }
