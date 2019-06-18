95aa0660c78fe207d1b9214ac7ffe75e17393d15
rkrumova <Rumyana.Krumova@endava.com>
2019-06-18 15:27:20 +0300

    US:000 Тест обйецт

---
+++ src/app/modules/core/query.dto.ts
@@ -0,0 +1,5 @@
+export class QueryDto {
+  s1: number;
+  s2: number;
+  s3: number;
+}
---
+++ src/app/modules/core/query.model.ts
@@ -0,0 +1,8 @@
+export class Query {
+  constructor(s1: number, s2: number ) {
+    this.s1 = s1;
+    this.s2 = s2;
+  }
+  s1: number;
+  s2: number;
+}
