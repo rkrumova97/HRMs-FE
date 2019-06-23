6c2d23ff1485a00d35fdee5f26baf0406af220bd
DESKTOP-B1A21RN\Master <kiro972@abv.bg>
2019-06-23 23:30:45 +0300

    Send mail with generated PDF for application for vacation

--- src/app/modules/employee/model/application-for-vacation.model.ts
+++ src/app/modules/employee/model/application-for-vacation.model.ts
@@ -1,10 +1,13 @@
 export class ApplicationForVacation {
   vacationDays: string;
   startDate: any;
+  hrName: string;
 
   constructor( vacationDays: string,
-               startDate: any) {
+               startDate: any,
+               hrName: string) {
     this.vacationDays = vacationDays;
     this.startDate = startDate;
+    this.hrName = hrName;
   }
 }
